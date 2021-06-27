7f3d30d080bc0cd48bf8441247eb8609e11e4860
sukhchainn <32098299+sukhchainn@users.noreply.github.com>
2021-06-27 09:18:36 +0530

    Now it correctly resizes

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -6,12 +6,12 @@ canvas.style.position = "absolute";
 canvas.style.border = "1px solid";
 
 const ctx = canvas.getContext('2d');
-const btn = document.getElementById('button');
+const btn = document.getElementById('_button');
 var image = document.createElement('img');
 var imageData;
 
-var x=0, y=0, width=600, height=300, rows=0, columns=0, size=5; 
-var rgbArray = new Array();
+var x=0, y=0, rows=0, columns=0, size=5; 
+var rgbArray = new Array(); 
 
 // btn.style.border = 'none';
 ctx.strokeStyle = 'black';
@@ -19,20 +19,35 @@ ctx.lineWidth = 5;
 
 
 function imgToCanvas() {
-  image.src = btn.style.backgroundImage.replace("url(\"", "").replace("\")", "");
+  let style = btn.currentStyle || window.getComputedStyle(btn, false);
+  image.src = style.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2').split(',')[0];
+
   ctx.drawImage(image,0,0);
 }
+
 function canvasToRgb() {
   imageData = ctx.getImageData(0, 0, image.width, image.height);
-  console.log(imageData);
   let counter = 0;
   for (let i=0; i<imageData.height; i++) {
     rgbArray.push(new Array());
     for (let j=0; j<imageData.width; j++) {
-        rgbArray[i].push(new RGBA(imageData.data[counter++], imageData.data[counter++], imageData.data[counter++], imageData.data[counter++]));
+      let r = imageData.data[counter++];
+      let g = imageData.data[counter++];
+      let b = imageData.data[counter++];
+      let a = imageData.data[counter++];
+
+      if (r == 255 &&
+          g == 255 &&
+          b == 255 &&
+          a == 255) {
+        rgbArray[i].push(new RGBA(r, g, b, 0));
+      } else {
+        rgbArray[i].push(new RGBA(r, g, b, a));
+      }
     }
   }
 }
+
 function stretchToHeight() {
   if (rows > rgbArray.length) {
     // if rows are more than rgbArray.length then add the number of deficient rows to rgbArray
@@ -50,6 +65,7 @@ function stretchToHeight() {
     rgbArray.splice(from, excess);
   }
 }
+
 function stretchToWidth() {
   if (columns > rgbArray[0].length) {
     // if rows are more than rgbArray.length then add the number of deficient rows to rgbArray
@@ -64,28 +80,30 @@ function stretchToWidth() {
     }
     console.log(rgbArray.length, rgbArray[0].length, rows, columns);
   } else {
-
     // if rows are less than rgbArray.length then subtract the number of excess rows from rgbArray
-    // let excess = rgbArray[0].length - rows;
-    // let from = rgbArray[0].length/2 - excess;
+    let excess = rgbArray[0].length - columns;
+    let from = rgbArray[0].length/2 - excess;
 
-    // rgbArray.splice(from, excess);
+    for (let i=0; i<rgbArray.length; i++) {
+      rgbArray[i].splice(from, excess);
+    }
   }
 }
 
 function matchRgbToBoundary() {
   x=0; y=0;
   // count rows and colums of btn.
+  columns--;
   while (y < btn.offsetHeight) {
-    if (y==0) {
-	    columns++;
-    }
     if (x > btn.offsetWidth) {
       x=0;
       y+=size;
       rows++;
     } else {
       x+=size;
+      if (y==0) {
+        columns++;
+      }
     }
   }
 
@@ -94,6 +112,7 @@ function matchRgbToBoundary() {
   stretchToHeight();
   stretchToWidth();
 }
+
 function rgbToCanvasScaled() {
   x=0; y=0;
   for (let i=0; i<rows; i++) {
@@ -107,9 +126,21 @@ function rgbToCanvasScaled() {
     y+=size;
   }
 }
+
+function clearCanvas() {
+  // Store the current transformation matrix
+  ctx.save();
+
+  // Use the identity matrix while clearing the canvas
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Restore the transform
+  ctx.restore();
+}
+
 function canvasToImg() {
-  imageData = ctx.getImageData(0, 0, 600, 300);
-  ctx.putImageData(imageData, 0, 0);
+  // imageData = ctx.getImageData(0, 0, image.width, image.height);
   var url = canvas.toDataURL();
 
   // var newImg = document.createElement("img"); // create img tag
@@ -118,11 +149,21 @@ function canvasToImg() {
   // document.body.appendChild(newImg); // add to end of your document
 }
 
+
 function draw(s) {
   size = s;
+
+  // put the background image into the canvas
 	imgToCanvas();
+  btn.style.background = 'transparent';
+  // convert the individual r, g, b, a values to RGB objects
   canvasToRgb();
+  // stretch the image to the element's size
   matchRgbToBoundary();
+  // clear the previous small image at the top left corner
+  clearCanvas();
+  // scale the individual pixels to the size and put them to canvas
   rgbToCanvasScaled();
+  // put the scaled image to the element's background
 	canvasToImg();
 }
